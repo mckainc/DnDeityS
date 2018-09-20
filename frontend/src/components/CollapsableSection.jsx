9a167e5027c215290cb4a95327fea5b41c22cc98
Nicholas Fordyce <nfordyc@purdue.edu>
2018-09-20 13:31:41 -0400

    Created collapsable section component

---
+++ frontend/src/components/CollapsableSection.jsx
@@ -0,0 +1,29 @@
+import React, { PureComponent } from 'react';
+import Collapse from 'react-bootstrap/lib/Collapse';
+
+class CollapsableSection extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            open: props.open,
+        }
+    }
+
+    toggleCollapse = () => {
+        this.setState({ open: !this.state.open });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 onClick={this.toggleCollapse}>{this.props.title}</h2>
+                <Collapse in={this.state.open}>
+                    <div>{this.props.children}</div>
+                </Collapse>
+            </div>
+        )
+    }
+}
+
+export default CollapsableSection;
