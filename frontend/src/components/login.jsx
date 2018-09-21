4e3bab01f4e1e5930e61a94f954216b332b1b6cf
cmckain <mckainc@comcast.net>
2018-09-20 20:03:18 -0400

    Framework for login jsx

---
+++ frontend/src/components/login.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import './login.css'
+
+export default class login extends Component {
+    render() {
+        return (
+            //header of page
+            <Grid>
+                <Jumbotron>
+                    <h1>DnDeity</h1>
+                    <p>personal reminder to update css</p>
+                </Jumbotron>
+            </Grid>
+            
+            //login button section
+        )
+    }
+}
