01ced8b669d479df541e24aec4c9f452a8e0bad2
Nicholas Fordyce <nfordyc@purdue.edu>
2018-09-24 15:40:24 -0400

    Add radio buttons to choose how to generate ability scores

--- frontend/src/pages/character-creator/ScoreSection.jsx
+++ frontend/src/pages/character-creator/ScoreSection.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 // components
-import { Col, Row } from 'react-bootstrap';
+import { Col, Radio, Row } from 'react-bootstrap';
 
 import AbilityScore from '../../components/AbilityScore';
 import CollapsableSection from '../../components/CollapsableSection';
@@ -9,10 +9,27 @@ import CollapsableSection from '../../components/CollapsableSection';
 const abilityScores = ['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA']
 
 class ScoreSection extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      manual: true,
+    }
+  }
+
+  setManual(manual) {
+    console.log(manual)
+    this.setState({ manual });
+  }
+
   render() {
+    const { manual } = this.state;
     return (
       <div className="ScoreSection">
         <CollapsableSection title="Ability Scores" open={true}>
+          <b>Score Generation Method:</b>
+          <Radio checked={manual} name="methodGroup" onClick={() => this.setManual(true)}>Manual</Radio>
+          <Radio checked={!manual} name="methodGroup" onClick={() => this.setManual(false)}>Point Buy</Radio>
           <Row>
             {abilityScores.map(score => (
               <Col xs={3} md={2}><AbilityScore type={score}/></Col>
