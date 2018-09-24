5038ca89a82c0582cf6db989618ce2a03bf0df90
Nicholas Fordyce <nfordyc@purdue.edu>
2018-09-24 16:33:29 -0400

    Add point buy method of calculating ability scores

--- frontend/src/components/AbilityScore.jsx
+++ frontend/src/components/AbilityScore.jsx
@@ -9,24 +9,36 @@ class AbilityScore extends Component {
 
     this.state = {
       modifier: -1,
+      score: props.score,
     }
   }
 
   updateModifier = (e) => {
-    const score = e.target.value;
-    const newModifier = Math.ceil((score - 10) / 2)
-    this.setState({ modifier: newModifier });
+    let score = e.target.value;
+  
+    // If using point buy, don't let scores be lower than 8 or higher than 15
+    if (!this.props.manual) {
+      if (score < 8) score = 8;
+      if (score > 15) score = 15;
+    }
+
+    // calculate new modifier
+    const modifier = Math.ceil((score - 10) / 2)
+    this.setState({ modifier, score });
+ 
+    // recalculate how many points are left
+    if(!this.props.manual) this.props.updatePoints(score, this.props.type);
   }
 
   render() {
     const { type } = this.props;
-    const { modifier } = this.state;
+    const { modifier, score } = this.state;
     return (
       <div className="AbilityScore">
         <b>{type}</b>
         <Panel>
           <Panel.Body>
-            <input type="number" name={`${type}-value`} id={type} defaultValue={8} onChange={this.updateModifier}/>
+            <input type="number" name={`${type}-value`} id={type} value={score} onChange={this.updateModifier}/>
           </Panel.Body>
         </Panel>
         {modifier >= 0 && <p>{`+${modifier}`}</p>}
--- frontend/src/pages/character-creator/ScoreSection.jsx
+++ frontend/src/pages/character-creator/ScoreSection.jsx
@@ -14,25 +14,52 @@ class ScoreSection extends Component {
 
     this.state = {
       manual: true,
+      pointTotal: 27,
+      points: [8, 8, 8, 8, 8, 8],
     }
   }
 
+  updatePoints = (score, type) => {
+    const { points } = this.state;
+    const index = abilityScores.indexOf(type);
+
+    let pointTotal = 27; // max amount of points
+    points[index] = score;
+
+    // calculate amount of points left
+    for(let i = 0; i < abilityScores.length;i++) {
+      pointTotal = pointTotal - (points[i] - 8);
+      // scores above 13 are more expensive
+      if(points[i] > 13) pointTotal--;
+      if(points[i] > 14) pointTotal--;
+    }
+
+    this.setState({ points, pointTotal});
+    this.forceUpdate();
+  }
+
   setManual(manual) {
-    console.log(manual)
     this.setState({ manual });
   }
 
   render() {
-    const { manual } = this.state;
+    const { manual, pointTotal } = this.state;
     return (
       <div className="ScoreSection">
         <CollapsableSection title="Ability Scores" open={true}>
           <b>Score Generation Method:</b>
           <Radio checked={manual} name="methodGroup" onClick={() => this.setManual(true)}>Manual</Radio>
           <Radio checked={!manual} name="methodGroup" onClick={() => this.setManual(false)}>Point Buy</Radio>
+          {!manual &&
+            <div>
+              <h3 class={pointTotal < 0 ? "text-danger" : ""}>
+                Points Remaining: {pointTotal}
+              </h3>
+            </div>
+          }
           <Row>
             {abilityScores.map(score => (
-              <Col xs={3} md={2}><AbilityScore type={score}/></Col>
+              <Col xs={3} md={2}><AbilityScore type={score} score={8} manual={manual} updatePoints={this.updatePoints}/></Col>
             ))}
           </Row>
         </CollapsableSection>
