b23180db9e678aec3263f16a29301436a3a877f6
Nicholas Fordyce <nfordyc@purdue.edu>
2018-09-24 12:03:32 -0400

    Add modifiers to ability score section

--- frontend/src/components/AbilityScore.jsx
+++ frontend/src/components/AbilityScore.jsx
@@ -4,14 +4,33 @@ import React, { Component } from 'react';
 import Panel from 'react-bootstrap/lib/Panel'
 
 class AbilityScore extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modifier: -1,
+    }
+  }
+
+  updateModifier = (e) => {
+    const score = e.target.value;
+    const newModifier = Math.ceil((score - 10) / 2)
+    this.setState({ modifier: newModifier });
+  }
+
   render() {
     const { type } = this.props;
+    const { modifier } = this.state;
     return (
       <div className="AbilityScore">
         <b>{type}</b>
         <Panel>
-          <Panel.Body><input type="number" name={`${type}-value`} id={type} defaultValue={8}/></Panel.Body>
+          <Panel.Body>
+            <input type="number" name={`${type}-value`} id={type} defaultValue={8} onChange={this.updateModifier}/>
+          </Panel.Body>
         </Panel>
+        {modifier >= 0 && <p>{`+${modifier}`}</p>}
+        {modifier < 0 && <p>{modifier}</p>}
       </div>
     );
   }
