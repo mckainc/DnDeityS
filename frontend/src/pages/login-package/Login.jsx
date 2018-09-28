0309a71faade8c47f316d73b7a5d72d848d5fbfc
cmckain <cmckain@purdue.edu>
2018-09-28 16:07:28 -0400

    Created login form js to handle user login

--- frontend/src/pages/login-package/Login.jsx
+++ frontend/src/pages/login-package/Login.jsx
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 import { FormControl } from 'react-bootstrap';
 
+import LoginForm from "./LoginForm"
+
 class Login extends Component {
     render(){
         return (
             <div className="Login">
                 <h1>Welcome to DnDeity</h1>
                 <h2>Please enter your login</h2>
+                <LoginForm />
             </div>
         );
     }
