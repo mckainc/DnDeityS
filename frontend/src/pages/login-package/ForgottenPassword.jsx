b333bcc29de225f94c8ef902aa3204dc7214190b
Nicholas Fordyce <nfordyc@purdue.edu>
2018-10-02 16:04:15 -0400

    Add empty field checking

--- frontend/src/pages/login-package/ForgottenPassword.jsx
+++ frontend/src/pages/login-package/ForgottenPassword.jsx
@@ -3,6 +3,34 @@ import React, { Component } from 'react';
 import { Panel, FormGroup, Form, FormControl, ControlLabel, Col, Button } from 'react-bootstrap';
 
 class ForgottenPassword extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      email: "",
+      displayError: false,
+    }
+  }
+
+  handleChange = e => {
+    console.log(e.target.name, e.target.value)
+    this.setState({
+        [e.target.name]: e.target.value
+    });
+  }
+
+  sendEmail = () => {
+    const { username, email } = this.state;
+    if(username === "" || email === "") {
+      console.log('error')
+      this.setState({ displayError: true });
+      return;
+    }
+
+    // TODO Send email
+  }
+
   render() {
     return (
       <div className="ForgottenPassword">
@@ -17,8 +45,10 @@ class ForgottenPassword extends Component {
                 </Col>
                 <Col sm={10}>
                   <FormControl
+                    name="username"
                     type="text"
                     placeholder="Enter Username"
+                    onChange={e => this.handleChange(e)}
                   />
                 </Col>
               </FormGroup>
@@ -28,13 +58,18 @@ class ForgottenPassword extends Component {
                 </Col>
                 <Col sm={10}>
                   <FormControl
+                    name="email"
                     type="email"
                     placeholder="Enter Email"
+                    onChange={e => this.handleChange(e)}
                   />
                 </Col>
               </FormGroup>
+              {this.state.displayError && 
+                <p>Please specify a username and email</p>
+              }
               <FormGroup>
-                <Button type="submit">Send Email</Button>
+                <Button onClick={this.sendEmail}>Send Email</Button>
               </FormGroup>
             </Form>
           </Panel.Body>
