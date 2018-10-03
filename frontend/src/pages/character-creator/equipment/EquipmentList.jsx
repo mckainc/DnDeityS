d0c03fe32dc65e996eb5075fea422315eb032d19
Nicholas Fordyce <nfordyc@purdue.edu>
2018-10-03 16:01:51 -0400

    Add basic search to equipment

--- frontend/src/pages/character-creator/equipment/EquipmentList.jsx
+++ frontend/src/pages/character-creator/equipment/EquipmentList.jsx
@@ -3,17 +3,39 @@ import React, { Component } from 'react';
 // components
 import EquipmentListItem from './EquipmentListItem';
 
-import { Well, ListGroup } from 'react-bootstrap';
+import { Well, ListGroup, FormControl } from 'react-bootstrap';
 
 class EquipmentList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchInput: "",
+    }
+  }
+
+  search = e => {
+    this.setState({ searchInput: e.target.value });
+  }
+
   render() {
+    const { searchInput } = this.state;
+    const filteredList = this.props.equipment.filter(item => {
+      return searchInput.toLowerCase() === item.name.substring(0, searchInput.length).toLowerCase();
+    });
     return (
       <div className="EquipmentList">
         <Well>
           <b>List of Equipment:</b>
+          <FormControl
+            name="Equipment Search"
+            type="text"
+            placeholder="Search for Equipment"
+            onChange={this.search}
+          />
           <div className="scrollable-list">
             <ListGroup>
-              {this.props.equipment.valueSeq().map(item => (
+              {filteredList.valueSeq().map(item => (
                 <EquipmentListItem item={item}/>
               ))}
             </ListGroup>
