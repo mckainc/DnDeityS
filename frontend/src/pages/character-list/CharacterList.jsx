f7db3b7216ad56d538618dc36edfcf7d9c0e6521
Nicholas Fordyce <nfordyc@purdue.edu>
2018-10-04 03:11:54 -0400

    Start character list page

--- frontend/src/pages/character-list/CharacterList.jsx
+++ frontend/src/pages/character-list/CharacterList.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+// components
+import SiteNavBar from '../../components/SiteNavBar';
+
+import './CharacterList.css';
 
 class CharacterList extends Component {
   render() {
     return (
       <div className="CharacterList">
-        <h1>Character List Page</h1>
+        <SiteNavBar/>
+        <div className="character-list-content">
+          <h1>Character List</h1>
+          <Link to="/CharacterCreater">
+            Character Creator
+          </Link>
+        </div>
       </div>
     );
   }
