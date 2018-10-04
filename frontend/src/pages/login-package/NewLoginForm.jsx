4c7806cb74510cb5012f67b8820d9df4d382d46c
cmckain <cmckain@purdue.edu>
2018-10-04 03:14:04 -0400

    Diffrent plan of attack for login form

--- frontend/src/pages/login-package/NewLoginForm.jsx
+++ frontend/src/pages/login-package/NewLoginForm.jsx
@@ -3,55 +3,59 @@ import { FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap
 
 class NewLoginForm extends React.Component {
 
-    constructor(props, context){
+    constructor(props, context) {
         super(props, context);
-
+    
         this.handleChange = this.handleChange.bind(this);
-
+        this.handleChangeP = this.handleChangeP.bind(this);
+    
         this.state = {
-            value: ''
+          value: '',
+          password: ''
         };
-
-    }
-
-    getValidationState(){
-        const length = this.state.value.lenght;
-        if(length == 0) 
-            return 'error';
-        else
-            return 'success';
-        
-    }
-
-    handleChange(e){
+      }
+    
+      getValidationState() {
+        const length = this.state.value.length;
+        if (length > 10) return 'success';
+        else if (length > 5) return 'warning';
+        else if (length > 0) return 'error';
+        return null;
+      }
+    
+      handleChange(e) {
         this.setState({ value: e.target.value });
-    }
+      }
 
-    render(){
+      handleChangeP(e){
+          this.setState({ password: e.target.password });
+      }
+    
+      render() {
         return (
-            <form>
-                <FormGroup
-                    controlId="loginText"
-                    validationState={this.getValidationState()}
-                >
-                    <FormControl
-                        type="text"
-                        value={this.state.value}
-                        placeholder="User Name"
-                        onChange={this.handleChange}
-                    />
-                    <FormControl
-                        type="text"
-                        value={this.state.value}
-                        placeholder="Password"
-                        onChange={this.handleChange}
-                    />
-                    <FormControl.Feedback />
-                    
-                </FormGroup>
-            </form>
+          <form>
+            <FormGroup
+              controlId="formBasicText"
+              validationState={this.getValidationState()}
+            >
+              <FormControl
+                type="text"
+                value={this.state.value}
+                placeholder="Enter text"
+                onChange={this.handleChange}
+              />
+
+              <FormControl
+                  type="text"
+                  value={this.state.password}
+                  placeholder="Password"
+                  onChange={this.handleChangeP}
+              />
+              <FormControl.Feedback />
+            </FormGroup>
+          </form>
         );
-    }
+      }
 }
 
 export default NewLoginForm;
