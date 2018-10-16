e25a5c728c1b88dbe03a823310b440d554275c72
Nicholas Fordyce <nfordyc@purdue.edu>
2018-10-16 11:55:15 -0400

    Icon Toolbar styling

--- frontend/src/pages/map-maker/IconToolbar.jsx
+++ frontend/src/pages/map-maker/IconToolbar.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 
 // components
-import { Button } from 'react-bootstrap';
+import { Button, Panel } from 'react-bootstrap';
+
+import './IconToolbar.css';
 
 class IconToolbar extends Component {
   render() {
     return (
       <div className="IconToolbar">
-        <Button ><i className="fas fa-pencil-alt" /></Button>
-        <br/>
-        <Button ><i className="fas fa-eraser" /></Button>
-        <br/>
-        <Button ><i className="fas fa-edit" /></Button>
+        <Panel>
+          <h4>Tools</h4>
+          <Button ><i className="fas fa-pencil-alt" /></Button>
+          <br/>
+          <Button ><i className="fas fa-eraser" /></Button>
+          <br/>
+          <Button ><i className="fas fa-edit" /></Button>
+        </Panel>
       </div>
     )
   }
