5c3eccedf0fc5d4b1bc962b751741098e4a73a18
Nicholas Fordyce <nfordyc@purdue.edu>
2018-10-18 11:58:28 -0400

    Select tool from toolbar

--- frontend/src/pages/map-maker/IconToolbar.jsx
+++ frontend/src/pages/map-maker/IconToolbar.jsx
@@ -7,15 +7,22 @@ import './IconToolbar.css';
 
 class IconToolbar extends Component {
   render() {
+    const { selectedTool } = this.props;
     return (
       <div className="IconToolbar">
         <Panel>
           <h4>Tools</h4>
-          <Button ><i className="fas fa-pencil-alt" /></Button>
+          <Button onClick={() => this.props.changeTool('draw')} active={selectedTool === 'draw'}>
+            <i className="fas fa-pencil-alt" />
+          </Button>
           <br/>
-          <Button ><i className="fas fa-eraser" /></Button>
+          <Button onClick={() => this.props.changeTool('erase')} active={selectedTool === 'erase'}>
+            <i className="fas fa-eraser" />
+          </Button>
           <br/>
-          <Button ><i className="fas fa-edit" /></Button>
+          <Button onClick={() => this.props.changeTool('edit')} active={selectedTool === 'edit'}>
+            <i className="fas fa-edit" />
+          </Button>
         </Panel>
       </div>
     )
--- frontend/src/pages/map-maker/MapEditor.jsx
+++ frontend/src/pages/map-maker/MapEditor.jsx
@@ -8,14 +8,26 @@ import TileSelector from './TileSelector';
 import { Col } from 'react-bootstrap';
 
 class MapEditor extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedTool: 'draw',
+    }
+  }
+
+  changeTool = (selectedTool) => {
+    this.setState({ selectedTool });
+  }
+
   render() {
     return (
       <div className="MapEditor">
         <Col md={1}>
-          <IconToolbar />
+          <IconToolbar changeTool={this.changeTool} selectedTool={this.state.selectedTool} />
         </Col>
         <Col md={9}>
-          <MapGrid x={25} y={25} />
+          <MapGrid x={25} y={25} selectedTool={this.state.selectedTool} />
         </Col>
         <Col md={2}>
           <TileSelector />
