7a0a7b94a60556f043df956a670d578a807cd08f
Nicholas Fordyce <nfordyc@purdue.edu>
2018-10-19 11:56:33 -0400

    Layer Toolbar style fixes

--- frontend/src/pages/map-maker/LayerToolbar.jsx
+++ frontend/src/pages/map-maker/LayerToolbar.jsx
@@ -3,22 +3,26 @@ import React, { Component } from 'react';
 // components
 import { Button, Panel } from 'react-bootstrap';
 
+import './LayerToolbar.css';
+
 class LayerToolbar extends Component {
   render() {
     const { changeLayer, selectedLayer } = this.props;
     return (
       <div className="LayerToolbar">
         <Panel>
-          <Button onClick={() => changeLayer('tiles')} active={selectedLayer === 'tiles'}>
-            <i className="fas fa-gavel" /> Tiles
-          </Button>
-          <Button onClick={() => changeLayer('enemies')} active={selectedLayer === 'enemies'}>
-            <i className="fas fa-skull" /> Enemies
-          </Button>
-          <Button onClick={() => changeLayer('events')} active={selectedLayer === 'events'}>
-            <i className="fas fa-exclamation" /> Events
-          </Button>
-          <Button><i className="fas fa-cog" /> Settings</Button>
+          <Panel.Body>
+            <Button onClick={() => changeLayer('tiles')} active={selectedLayer === 'tiles'}>
+              <i className="fas fa-gavel" /> Tiles
+            </Button>
+            <Button onClick={() => changeLayer('enemies')} active={selectedLayer === 'enemies'}>
+              <i className="fas fa-skull" /> Enemies
+            </Button>
+            <Button onClick={() => changeLayer('events')} active={selectedLayer === 'events'}>
+              <i className="fas fa-exclamation" /> Events
+            </Button>
+            <Button className="settings"><i className="fas fa-cog" /> Settings</Button>
+          </Panel.Body>
         </Panel>
       </div>
     )
