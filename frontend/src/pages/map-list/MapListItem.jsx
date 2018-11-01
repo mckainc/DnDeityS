e23a117e8f70671ec794194df0750f2aaf0f5883
Nicholas Fordyce <nfordyc@purdue.edu>
2018-11-01 14:37:36 -0400

    Fix Edit Button on Map List Page

--- frontend/src/pages/map-list/MapListItem.jsx
+++ frontend/src/pages/map-list/MapListItem.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 // components
 import { Panel, Button, Col } from 'react-bootstrap';
 
 class MapListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      redirect: false,
+    }
+  }
+
+  handleClick = () => {
+    this.setState({ redirect: true });
+  }
+
   render() {
     const { map } = this.props;
+    if (this.state.redirect) {
+      return <Redirect to = {'/MapMaker/' + map.id}/>
+    }
     return (
       <div className="MapListItem">
         <Col sm={4}>
@@ -15,7 +30,7 @@ class MapListItem extends Component {
             <Panel.Body>
               <p><b>Height: </b>{map.height}</p>
               <p><b>Width: </b>{map.width}</p>
-              <Button><Link to={'/MapMaker/' + map.id}>Edit</Link></Button>
+              <Button onClick={this.handleClick}>Edit</Button>
               <Button bsStyle="danger" onClick={() => this.props.deleteMap(map.id)}>Delete</Button>
             </Panel.Body>
           </Panel>
