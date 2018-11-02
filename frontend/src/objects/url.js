7d9e95a5d1cdd4e0df5f89b27925f4234451a160
rileyturnbull <ribull44@gmail.com>
2018-11-02 15:40:40 -0400

    test documentation

--- frontend/src/objects/url.js
+++ frontend/src/objects/url.js
@@ -1,3 +1,4 @@
-const serverURL = 'http://prod.vs23jhxnvj.us-east-2.elasticbeanstalk.com';
+// const serverURL = 'http://prod.vs23jhxnvj.us-east-2.elasticbeanstalk.com';
+const serverURL = 'http://127.0.0.1:5000'
 
 export default serverURL;
