83d491c96278d518c3d5d0ab51f303da89641e74
rileyturnbull <ribull44@gmail.com>
2018-11-01 20:51:50 -0400

    finished map loading test

--- testing/map_maker_test.py
+++ testing/map_maker_test.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 import mysql.connector
 
 def generate_map(x, y):
@@ -112,8 +113,6 @@ class MapMakerTest(unittest.TestCase):
 		# generate random map to create
 		x = random.randint(1, 20)
 		y = random.randint(1, 20)
-		x = 5
-		y = 8
 		test_map = generate_map(x, y)
 
 		# print the map
@@ -127,6 +126,7 @@ class MapMakerTest(unittest.TestCase):
 			self.assertTrue(False)
 			return
 		elem.click()
+		time.sleep(1)
 		try:
 			inputs = driver.find_elements_by_tag_name('input')
 			inputs[0].clear()
@@ -183,6 +183,7 @@ class MapMakerTest(unittest.TestCase):
 		# save
 		elem = driver.find_element_by_class_name("save")
 		elem.click()
+		time.sleep(2)
 
 		# check for correct saving
 		# convert my map to db structure
@@ -203,6 +204,89 @@ class MapMakerTest(unittest.TestCase):
 		db.commit()
 		cur.close()
 		db.close()
+
+	def test_map_is_loaded_correctly(self):
+		driver = self.driver
+		driver.get("localhost:3000/")
+		self.assertIn("React App", driver.title)
+		elem = driver.find_element_by_id('username')
+		elem.send_keys("username")
+		elem = driver.find_element_by_id('password')
+		elem.send_keys("password")
+		elem.submit()
+
+		# test that the page properly loaded
+		time.sleep(1)
+		try:
+			elem = driver.find_element(By.TAG_NAME, 'h2')
+		except Exception as e:
+			self.assertTrue(False)
+			return
+
+		# generate map to test against
+		x = random.randint(1, 20)
+		y = random.randint(1, 20)
+		test_map = generate_map(x, y)
+		print_map(test_map, x, y)
+		my_map = convert_to_db_map(test_map, x, y)
+
+		# insert into database
+		db_dnd_host = '127.0.0.1'
+		db_dnd_user = 'root'
+		db_dnd_password = 'password'
+		db_dnd = 'dnd'
+		db = mysql.connector.connect(host=db_dnd_host, user=db_dnd_user, password=db_dnd_password, database=db_dnd)
+		cur = db.cursor()
+		cur.execute("insert into Maps (UserId, MapName, MapHeight, MapWidth, MapTiles) values (4, 'test_map_2', " + str(x) + ", " + str(y) + ", '" + str(json.dumps(my_map)) + "')")
+		db.commit()
+
+		# does map selector exist?
+		try:
+			elem = driver.find_element_by_link_text('Maps')
+		except Exception as e:
+			self.assertTrue(False)
+			return
+		elem.click()
+		time.sleep(2)
+
+		elem = driver.find_element_by_xpath("//div[contains(text(), 'test_map_2')]//parent::div//button")
+		elem.click()
+		time.sleep(2)
+
+		# check for equality
+		display_map = []
+		tiles = driver.find_elements_by_class_name("MapTile")
+		for i in range(x):
+			for j in range(y):
+				added = dict()
+				try:
+					tile = tiles[(y * i) + j].find_element_by_xpath(".//img")
+					ttype = tile.get_attribute("src")
+					if "grass" in ttype:
+						added['tile'] = 'grass'
+					elif "stone" in ttype:
+						added['tile'] = 'stone'
+					elif "dirt" in ttype:
+						added['tile'] = 'dirt'
+					elif "wood" in ttype:
+						added['tile'] = 'wood'
+					if tile.get_attribute("class") == "monster":
+						added['monster'] = {'name':'', 'description':'', 'type':''}
+					elif tile.get_attribute("class") == "event":
+						added['event'] = {'name':'', 'description':''}
+					added['x'] = i
+					added['y'] = j
+					display_map.append(added)
+				except NoSuchElementException as e:
+					pass
+		# print(display_map)
+		# print(my_map)
+		self.assertEqual(display_map, my_map)
+		cur.execute("delete from Maps where MapName = 'test_map_2'")
+		db.commit()
+		cur.close()
+		db.close()
+
 
 	def tearDown(self):
 		return
