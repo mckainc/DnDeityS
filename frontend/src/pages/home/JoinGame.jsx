2404f0177cff0065f725553f7346be2d8b214c2b
Nicholas Fordyce <nfordyc@purdue.edu>
2018-11-15 15:04:08 -0500

    Join Game component sends message

--- frontend/src/pages/home/JoinGame.jsx
+++ frontend/src/pages/home/JoinGame.jsx
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+// types
+import serverURL from '../../objects/url.js';
 
 // components
 import { Modal, Form, FormControl, Col, ControlLabel, Button, FormGroup } from 'react-bootstrap';
 
 class JoinGame extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      code: '',
+    }
+  }
+
+  handleChange = e => {
+    this.setState({ code: e.target.value });
+  }
+
+  join = () => {
+    const server = axios.create({
+      baseURL: serverURL,
+    });
+
+    const json = {
+      channel: this.state.code,
+      event: 'join-lobby',
+      message: {
+        msg: 'user joined'
+      }
+    }
+
+    server.post('/pushmessage', JSON.stringify(json));
+
+    // TODO bind channel to events
+  }
+
   render() {
     return (
       <Modal className="JoinGame" show={this.props.showJoinModal} onHide={this.props.onClose}>
@@ -19,10 +53,11 @@ class JoinGame extends Component {
                 name="code"
                 type="text"
                 placeholder="Enter Lobby Code"
+                onChange={this.handleChange}
               />
             </Col>
             <Col sm={2}>
-              <Button>Join</Button>
+              <Button onClick={this.join}>Join</Button>
             </Col>
           </FormGroup>
         </Form>
