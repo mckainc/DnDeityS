d60408d193944b0fa257ab5d6671d9bec7f7d2b1
Nicholas Fordyce <nfordyc@purdue.edu>
2018-11-24 21:26:25 -0500

    Start game screen

---
+++ frontend/src/pages/game/Game.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+// types
+import { Map } from 'immutable';
+
+// components
+import MapGrid from '../../pages/map-maker/MapGrid';
+import { Col } from 'react-bootstrap';
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      map: new Map(),
+    }
+  }
+
+  render() {
+    return (
+      <div className="Game">
+        <Col md={10}>
+          <MapGrid x={25} y={25} map={this.state.map} playing={true} />
+        </Col>
+        <Col md={2}>
+          Character Component
+        </Col>
+      </div>
+    )
+  }
+}
+
+export default Game;
