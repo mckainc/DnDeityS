8d7c0974c2b3cb5150b9db15def9578391fc1fe1
cmckain <cmckain@purdue.edu>
2018-11-27 15:58:06 -0500

    Can roll several dice

--- frontend/src/components/DiceTray.jsx
+++ frontend/src/components/DiceTray.jsx
@@ -37,6 +37,12 @@ class DiceTray extends Component {
       roll10: 1,
       roll12: 1,
       roll20: 1,
+      num4: 1,
+      num6: 1,
+      num8: 1,
+      num10: 1,
+      num12: 1,
+      num20: 1,
     }
   }
 
@@ -48,25 +54,35 @@ class DiceTray extends Component {
     this.setState({ showP1: false});
   }
   
-  handleDiceRoll(die, bonus, id, realDie) {
-    var roll = Math.floor(Math.random() * (parseInt(die))) + 1;
+  handleDiceRoll(die, bonus, id, realDie, numDie) {
+    if(bonus===''){
+      bonus = 0;
+    }
+
+    var roll = 0;
+    if(numDie < 1){
+      numDie = 1;
+    }
+    var d1 = Math.floor(Math.random() * (parseInt(die))) + 1;
+    for(var x = 0;x < parseInt(numDie)-1;x++){
+       roll = roll + Math.floor(Math.random() * (parseInt(die))) + 1;
+    }
+    roll = roll + d1;//add the one I took away
     var bon = bonus;
     let result = parseInt(roll) + parseInt(bon);
     this.setState({[id]: result});
-    this.setState({[realDie]: roll});
+    this.setState({[realDie]: d1});//only shows the first die rolled
   }
 
   handleChange = event => {
-
     const re = /^[0-9\b]+$/;
       if (event.target.value === '' || re.test(event.target.value)) {
-         this.setState({[event.target.id]: event.target.value})
+         this.setState({[event.target.id]: event.target.value});
       }
   }
 
-  render() {
 
-    const pathD4 = "../textures/dice/d4";
+  render() {
     
     return (
       <div className="DiceModal">
@@ -83,19 +99,32 @@ class DiceTray extends Component {
             <Modal.Body>
 
                 <Form bsClass="help">
-              <FormGroup controlId="bonus4">
+              
                 <Row>
                   <Col sm={2}>
-                    <Button onClick={() => this.handleDiceRoll(4,this.state.bonus4,"res4","roll4")}>
+                    <Button onClick={() => this.handleDiceRoll(4,this.state.bonus4,"res4","roll4",this.state.num4)}>
                      <img src={require("../textures/dice/d4/" + this.state.roll4 + ".png")}/>
                      </Button>
                    </Col>
+
+                   <Col sm={2}>
+                   <FormGroup controlId="num4">
+                      <FormControl controlId="num4"
+                        bsClass="bonusField"
+                        autoFocus
+                        type="text"
+                        value={this.state.num4}
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    </Col>
 
                    <Col sm={2}>
                      <p>+</p>
                    </Col>
 
                    <Col sm={2}>
+                   <FormGroup controlId="bonus4">
                       <FormControl
                         bsClass="bonusField"
                         autoFocus
@@ -103,34 +132,51 @@ class DiceTray extends Component {
                         value={this.state.bonus4}
                         onChange={this.handleChange}
                       />
+                    </FormGroup>
                     </Col>
 
                     <Col sm={2}> 
                       <p>{this.state.res4}</p>
                     </Col>
                 </Row>
-               </FormGroup>
+              
 
               <FormGroup controlId="bonus6">
                 <Row>
                 <Col sm={2}>
-                <Button onClick={() => this.handleDiceRoll(6,this.state.bonus6, "res6","roll6")}>
+                <Button onClick={() => this.handleDiceRoll(6,this.state.bonus6, "res6","roll6",this.state.num6)}>
                 <img src={require("../textures/dice/d6/" + this.state.roll6 + ".png")}/>
                 </Button>
                 </Col>
+
                 <Col sm={2}>
-                <p>+</p>
-                </Col>
-                <Col sm={2}>
-                <FormControl
-                  bsClass="bonusField"
-                  autoFocus
-                  type="text"
-                  pattern="[0-9]*"
-                  value={this.state.bonus6}
-                  onChange={this.handleChange}
-                />
-                </Col>
+                   <FormGroup controlId="num6">
+                      <FormControl controlId="num6"
+                        bsClass="bonusField"
+                        autoFocus
+                        type="text"
+                        value={this.state.num6}
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    </Col>
+
+                   <Col sm={2}>
+                     <p>+</p>
+                   </Col>
+
+                   <Col sm={2}>
+                   <FormGroup controlId="bonus6">
+                      <FormControl
+                        bsClass="bonusField"
+                        autoFocus
+                        type="text"
+                        value={this.state.bonus6}
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    </Col>
+
                 <Col sm={2}>
                 <p>{this.state.res6}</p>
                 </Col>
@@ -140,23 +186,39 @@ class DiceTray extends Component {
                <FormGroup controlId="bonus8">
                <Row>
                <Col sm={2}>
-                <Button onClick={() => this.handleDiceRoll(8,this.state.bonus8,"res8","roll8")}>
+                <Button onClick={() => this.handleDiceRoll(8,this.state.bonus8,"res8","roll8",this.state.num8)}>
                 <img src={require("../textures/dice/d8/" + this.state.roll8 + ".png")}/>
                 </Button>
                 </Col>
+
                 <Col sm={2}>
-                <p>+</p>
-                </Col>
-                <Col sm={2}>
-                <FormControl
-                  bsClass="bonusField"
-                  autoFocus
-                  type="text"
-                  pattern="[0-9]*"
-                  value={this.state.bonus8}
-                  onChange={this.handleChange}
-                />
-                </Col>
+                   <FormGroup controlId="num8">
+                      <FormControl controlId="num8"
+                        bsClass="bonusField"
+                        autoFocus
+                        type="text"
+                        value={this.state.num8}
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    </Col>
+
+                   <Col sm={2}>
+                     <p>+</p>
+                   </Col>
+
+                   <Col sm={2}>
+                   <FormGroup controlId="bonus8">
+                      <FormControl
+                        bsClass="bonusField"
+                        autoFocus
+                        type="text"
+                        value={this.state.bonus8}
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    </Col>
+
                 <Col sm={2}>
                 <p>{this.state.res8}</p>
                 </Col>
@@ -166,23 +228,39 @@ class DiceTray extends Component {
                <FormGroup controlId="bonus10">
                <Row>
                <Col sm={2}>
-                <Button onClick={() => this.handleDiceRoll(10,this.state.bonus10,"res10","roll10")}>
+                <Button onClick={() => this.handleDiceRoll(10,this.state.bonus10,"res10","roll10",this.state.num10)}>
                 <img src={require("../textures/dice/d10/" + this.state.roll10 + ".png")}/>
                 </Button>
                 </Col>
+
                 <Col sm={2}>
-                <p>+</p>
-                </Col>
-                <Col sm={2}>
-                <FormControl
-                  bsClass="bonusField"
-                  autoFocus
-                  type="text"
-                  pattern="[0-9]*"
-                  value={this.state.bonus10}
-                  onChange={this.handleChange}
-                />
-                </Col>
+                   <FormGroup controlId="num10">
+                      <FormControl controlId="num10"
+                        bsClass="bonusField"
+                        autoFocus
+                        type="text"
+                        value={this.state.num10}
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    </Col>
+
+                   <Col sm={2}>
+                     <p>+</p>
+                   </Col>
+
+                   <Col sm={2}>
+                   <FormGroup controlId="bonus10">
+                      <FormControl
+                        bsClass="bonusField"
+                        autoFocus
+                        type="text"
+                        value={this.state.bonus10}
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    </Col>
+
                 <Col sm={2}>
                 <p>{this.state.res10}</p>
                 </Col>
@@ -192,23 +270,39 @@ class DiceTray extends Component {
                <FormGroup controlId="bonus12">
                <Row>
                <Col sm={2}>
-                <Button onClick={() => this.handleDiceRoll(12,this.state.bonus12,"res12","roll12")}>
+                <Button onClick={() => this.handleDiceRoll(12,this.state.bonus12,"res12","roll12",this.state.num12)}>
                   <img src={require("../textures/dice/d12/" + this.state.roll12 + ".png")}/>
                 </Button>
                 </Col>
+
                 <Col sm={2}>
-                <p>+</p>
-                </Col>
-                <Col sm={2}>
-                <FormControl
-                  bsClass="bonusField"
-                  autoFocus
-                  type="text"
-                  pattern="[0-9]*"
-                  value={this.state.bonus12}
-                  onChange={this.handleChange}
-                />
-                </Col>
+                   <FormGroup controlId="num12">
+                      <FormControl controlId="num12"
+                        bsClass="bonusField"
+                        autoFocus
+                        type="text"
+                        value={this.state.num12}
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    </Col>
+
+                   <Col sm={2}>
+                     <p>+</p>
+                   </Col>
+
+                   <Col sm={2}>
+                   <FormGroup controlId="bonus12">
+                      <FormControl
+                        bsClass="bonusField"
+                        autoFocus
+                        type="text"
+                        value={this.state.bonus12}
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    </Col>
+
                 <Col sm={2}>
                 <p>{this.state.res12}</p>
                 </Col>
@@ -218,23 +312,39 @@ class DiceTray extends Component {
                <FormGroup controlId="bonus20">
                <Row>
                <Col sm={2}>
-                <Button onClick={() => this.handleDiceRoll(20,this.state.bonus20,"res20","roll20")}>
+                <Button onClick={() => this.handleDiceRoll(20,this.state.bonus20,"res20","roll20",this.state.num20)}>
                   <img src={require("../textures/dice/d20/" + this.state.roll20 + ".png")}/>
                 </Button>
                 </Col>
+
                 <Col sm={2}>
-                <p>+</p>
-                </Col>
-                <Col sm={2}>
-                <FormControl
-                  bsClass="bonusField"
-                  autoFocus
-                  type="text"
-                  pattern="[0-9]*"
-                  value={this.state.bonus20}
-                  onChange={this.handleChange}
-                />
-                </Col>
+                   <FormGroup controlId="num20">
+                      <FormControl controlId="num20"
+                        bsClass="bonusField"
+                        autoFocus
+                        type="text"
+                        value={this.state.num20}
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    </Col>
+
+                   <Col sm={2}>
+                     <p>+</p>
+                   </Col>
+
+                   <Col sm={2}>
+                   <FormGroup controlId="bonus20">
+                      <FormControl
+                        bsClass="bonusField"
+                        autoFocus
+                        type="text"
+                        value={this.state.bonus20}
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    </Col>
+
                 <Col sm={2}>
                 <p>{this.state.res20}</p>
                 </Col>
