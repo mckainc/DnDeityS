0c4297105359f7dac01d8d5f0c333b0682e6a8a3
cmckain <cmckain@purdue.edu>
2018-11-29 13:28:46 -0500

    Sence Section up and running, connected to MonsterMaker

--- frontend/src/pages/monster-maker/MonsterMaker.jsx
+++ frontend/src/pages/monster-maker/MonsterMaker.jsx
@@ -11,6 +11,7 @@ import CharacterNavBar from '../../components/CharacterNavBar';
 import DescriptionSection from './DescriptionSection';
 import CombatSection from './CombatSection';
 import AbilityScoreSection from './AbilityScoreSection';
+import SencesSection from './SencesSection';
 
 
 
@@ -63,6 +64,9 @@ class MonsterMaker extends Component {
               monster.special_abilities = JSON.parse(response.data[18]);
               monster.actions = JSON.parse(response.data[19]);
               monster.legendary_actions = JSON.parse(response.data[20]);
+              monster.passivePerception = response.data[21];
+              monster.vision1 = response.data[22];
+              monster.vision2 = response.data[23];
               this.setState({ monster, loaded: true });
             })
         }
@@ -97,7 +101,8 @@ class MonsterMaker extends Component {
                              <h1>Monster Maker</h1>
                              <DescriptionSection ref={this.state.refs[0]} changeMonster={this.changeMonster} monster={monster} loaded={loaded}/>
                              <CombatSection ref={this.state.refs[1]} changeMonster={this.changeMonster} monster={monster} loaded={loaded}/>
-                             <AbilityScoreSection ref={this.state.refs[1]} changeMonster={this.changeMonster} monster={monster} loaded={loaded}/>
+                             <AbilityScoreSection ref={this.state.refs[2]} changeMonster={this.changeMonster} monster={monster} loaded={loaded}/>
+                             <SencesSection ref={this.state.refs[3]} changeMonster={this.changeMonster} monster={monster} loaded={loaded}/>
                         </Col>
                     </Row>
                   </Grid>
